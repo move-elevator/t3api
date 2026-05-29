08169fd3462dccabe48e9d8b6702fe45bc53c0f1
Ronny Hauptvogel <rh@move-elevator.de>
2026-05-29 14:56:42 +0200

    refactor: add readonly type to ConditionFunctionsProvider to fit TYPO3 14 readonly Typo3ConditionFunctionsProvider

--- Classes/ExpressionLanguage/ConditionFunctionsProvider.php
+++ Classes/ExpressionLanguage/ConditionFunctionsProvider.php
@@ -6,7 +6,7 @@ namespace SourceBroker\T3api\ExpressionLanguage;
 
 use TYPO3\CMS\Core\ExpressionLanguage\FunctionsProvider\Typo3ConditionFunctionsProvider;
 
-class ConditionFunctionsProvider extends Typo3ConditionFunctionsProvider
+readonly class ConditionFunctionsProvider extends Typo3ConditionFunctionsProvider
 {
     // @TODO This comment is here just for reformatting compatibility of csfixes and phpstorm.
 }
